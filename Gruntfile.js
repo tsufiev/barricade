c42a95e5d35457e335dd9a785d33816aa3b77ce4
drag7507 <drago.rosson@rackspace.com>
2014-10-06 19:15:07 -0500

    Update copyright

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Copyright 2014 Drago Rosson
+// Copyright 2014 Rackspace
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 module.exports = function (grunt) {
     var apache_license = [
-            '// Copyright 2014 Drago Rosson',
+            '// Copyright 2014 Rackspace',
             '//',
             '// Licensed under the Apache License, Version 2.0 (the "License");',
             '// you may not use this file except in compliance with the License.',
