9b0d4368ccbf7de7fc94c1c8b59d5aecb36f0c97
Drago Rosson <drago@rosson.me>
2015-03-16 13:15:56 -0500

    Fix logError()

--- barricade.js
+++ barricade.js
@@ -1316,8 +1316,8 @@ var Barricade = (function () {
     }());
 
     function logError() {
-        console.error.apply(console, Array.prototype.slice.call(arguments)
-                                          .unshift('Barricade: '));
+        var args = Array.prototype.slice.call(arguments);
+        console.error.apply(console, ['Barricade: '].concat(args));
     }
 
     BarricadeMain = {
